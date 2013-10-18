cfdcac15e2f9541b46923f5b2664d5a2ab0c9395
Larry <llaski@resolute.com>
2013-10-18 17:59:55 -0400

    laravel testing

--- app/config/app.php
+++ app/config/app.php
@@ -65,7 +65,7 @@ return array(
 	|
 	*/
 
-	'key' => 'YourSecretKey!!!',
+	'key' => 'IN0JZtp55nDtWvngDs9HtFnZka5cpYFK',
 
 	/*
 	|--------------------------------------------------------------------------
---
+++ app/views/home/about.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>Laravel PHP Framework</title>
+   	<meta name="viewport" content="widget=device-width">
+</head>
+<body>
+	About Me
+</body>
+</html>
