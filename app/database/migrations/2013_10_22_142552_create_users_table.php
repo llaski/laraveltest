dbf477ebf8cd4aa6ee0d75c848f17ded229abbc9
Larry <llaski@resolute.com>
2013-10-22 18:02:06 -0400

    migrations

---
+++ app/database/migrations/2013_10_22_142552_create_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function($table){
+			$table->increments('id');
+			$table->string('name', 50);
+			$table->integer('age')->nullable();
+			$table->string('password', 40);
+			$table->boolean('active')->default(0);
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
